587cbff50d45aaa7e9754372b14c6e70f9a33e70
StJester <stellarjester@gmail.com>
2017-11-22 02:18:12 +0200

    Handler for journal update

--- SirRogue_alpha_v1.01/Form1.cs
+++ SirRogue_alpha_v1.01/Form1.cs
@@ -12,12 +12,20 @@ namespace SirRogue_alpha_v1._01
 {
     public partial class Form1 : Form
     {
+        List<Button> DirectionButtons = new List<Button>();
         public Form1()
         {
             InitializeComponent();
+            DirectionButtons.Add(NButton);
+            DirectionButtons.Add(NEButton);
+            DirectionButtons.Add(EButton);
+            DirectionButtons.Add(SEButton);
+            DirectionButtons.Add(SButton);
+            DirectionButtons.Add(SWButton);
+            DirectionButtons.Add(WButton);
+            DirectionButtons.Add(NWButton);
 
-
-           //ONE handler for all
+            //ONE handler for all
             NButton.Click += DirectionButtonClick;
             NEButton.Click += DirectionButtonClick;
             EButton.Click += DirectionButtonClick;
@@ -47,6 +55,63 @@ namespace SirRogue_alpha_v1._01
             
             MessageBox.Show($"Clicked on {button.Text}");
 
+        }
+        public void JournalUpdate(Quest q)//called twice - when first accepted and when accomplished
+        {
+            if(q.Completed)
+            {
+                CompletedQuests.Items.Add(q.Title);
+                return;
+            }
+            else
+            {
+                if(CurrentQuest.Text != null)//This one sucks
+                {
+                    MessageBox.Show("Already have quest!");
+                    return;
+                }
+                else
+                {
+                    CurrentQuest.Text = q.Text;
+
+                    //make every button.enabled == False
+                    foreach (var b in DirectionButtons)
+                    {
+                        if (b.Enabled)
+                        {
+                            b.Enabled = false;
+                        }
+                    }
+
+                    switch (q.Direction)
+                    {
+                        case "W": WButton.Enabled = true;
+                            break;
+                        case "N":
+                            NButton.Enabled = true;
+                            break;
+                        case "NE":
+                            NEButton.Enabled = true;
+                            break;
+                        case "NW":
+                            NWButton.Enabled = true;
+                            break;
+                        case "SE":
+                            SEButton.Enabled = true;
+                            break;
+                        case "SW":
+                            SWButton.Enabled = true;
+                            break;
+                        case "S":
+                            SButton.Enabled = true;
+                            break;
+                        case "E":
+                            EButton.Enabled = true;
+                            break;
+
+                    }
+                }
+            }
         }
 
     }
---
+++ SirRogue_alpha_v1.01/Quest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SirRogue_alpha_v1._01
+{
+    public class Quest
+    {
+        public string Title;
+        public string Text;
+        public bool Completed;
+        public string Direction;
+
+    }
+}
